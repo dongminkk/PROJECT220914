4c93948ea84518a20e4c6545acabaf47ca1eb3f7
dongminkk <beagleeg12@daum.net>
2022-09-19 20:41:40 +0900

    new file

---
+++ CircleArea.java
@@ -0,0 +1,13 @@
+//2017038093 김동민
+public class CircleArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI = 3.14; // 원주율
+		
+		double radius = 10.0; //반지름
+		double circleArea = radius * radius * PI; // 원의 면적 계산
+
+		System.out.println("원의 면적 = " + circleArea);
+	}
+}
